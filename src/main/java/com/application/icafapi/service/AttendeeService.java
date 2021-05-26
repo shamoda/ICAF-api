aae3a9f97507d3fed2d1bf1880e10baff0fc5a7f
shamoda <it19161648@my.sliit.lk>
2021-05-26 09:27:09 +0530

    email service implemented

--- src/main/java/com/application/icafapi/service/AttendeeService.java
+++ src/main/java/com/application/icafapi/service/AttendeeService.java
@@ -1,25 +1,31 @@
 package com.application.icafapi.service;
 
+import com.application.icafapi.common.util.EmailUtil;
 import com.application.icafapi.model.Attendee;
 import com.application.icafapi.model.User;
 import com.application.icafapi.repository.AttendeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static com.application.icafapi.common.util.constant.Email.*;
+
 @Service
 public class AttendeeService {
 
     private final AttendeeRepository repository;
     private final UserService userService;
+    private final EmailUtil emailUtil;
 
     @Autowired
-    public AttendeeService(AttendeeRepository repository, UserService userService) {
+    public AttendeeService(AttendeeRepository repository, UserService userService, EmailUtil emailUtil) {
         this.repository = repository;
         this.userService = userService;
+        this.emailUtil = emailUtil;
     }
 
     public Attendee insertAttendee(Attendee attendee, User user) {
         userService.insertUser(user);
+        emailUtil.sendEmail(user.getEmail(), USER_REGISTRATION_SUBJECT, ATTENDEE_REGISTRATION_BODY);
         return repository.save(attendee);
     }
 }
--- src/main/java/com/application/icafapi/service/ResearcherService.java
+++ src/main/java/com/application/icafapi/service/ResearcherService.java
@@ -1,5 +1,6 @@
 package com.application.icafapi.service;
 
+import com.application.icafapi.common.util.EmailUtil;
 import com.application.icafapi.model.Researcher;
 import com.application.icafapi.model.User;
 import com.application.icafapi.repository.ResearcherRepository;
@@ -7,23 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import static com.application.icafapi.common.util.constant.Email.*;
+
 @Service
 public class ResearcherService {
 
     private final ResearcherRepository repository;
     private final UserService userService;
     private final FileService fileService;
+    private final EmailUtil emailUtil;
 
     @Autowired
-    public ResearcherService(ResearcherRepository repository, UserService userService, FileService fileService) {
+    public ResearcherService(ResearcherRepository repository, UserService userService, FileService fileService, EmailUtil emailUtil) {
         this.repository = repository;
         this.userService = userService;
         this.fileService = fileService;
+        this.emailUtil = emailUtil;
     }
 
     public Researcher insertResearcher(Researcher researcher, User user, MultipartFile mFile) {
         fileService.uploadFile(mFile, researcher.getFileName(), "paper");
         userService.insertUser(user);
+        emailUtil.sendEmail(user.getEmail(), USER_REGISTRATION_SUBJECT, RESEARCHER_REGISTRATION_BODY);
         return repository.save(researcher);
     }
 
