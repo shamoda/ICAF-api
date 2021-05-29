d68671e12d502c77ab74fd7b739475a71c21df4d
Senath99 <senathweerasingha@gmail.com>
2021-05-29 20:50:33 +0530

    Worksop First commit

--- src/main/java/com/application/icafapi/common/constant/AWS.java
+++ src/main/java/com/application/icafapi/common/constant/AWS.java
@@ -5,8 +5,8 @@ import software.amazon.awssdk.regions.Region;
 public class AWS {
 
     public static final Region REGION = Region.US_EAST_1;
-    public static final String PAPER_BUCKET = "icaf-2021-paperss";
-    public static final String PROPOSAL_BUCKET = "icaf-2021-proposalss";
+    public static final String PAPER_BUCKET = "icaf-2021-papers";
+    public static final String PROPOSAL_BUCKET = "icaf-2021-proposals";
     public static final String TEMPLATE_BUCKET = "icaf-2021-templates";
 
     private AWS() {}
